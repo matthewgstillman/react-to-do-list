8ae24a8ae67a620fea103a2d6b96d4c8d226371a
matthewgstillman <matthewgstillman@gmail.com>
2022-05-11 12:48:40 -0600

    Latest commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 
-const Todo = ({ todo, index, completeTodo, undoComplete, removeTodo }) => {
+const Todo = ({ todo, index, completeTodo, removeTodo }) => {
   return (
     <div
       className="todo"
